571db8ee92b279ce3bd03fccc04a6f12ce935cf0
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2024-08-11 11:55:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 //Routes
 app.use('/product', productRoute);
 
