7de8df5d8cc06a215bd8bbff540c58dc0d9875f4
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2024-08-11 11:50:49 +0530

    updated

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const productRoute = require('./routes/product.route');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.set('view engine', 'ejs');
 //Routes
 app.use('/product', productRoute);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index.ejs', { name: 'Express' });
 });
 
 mongoose
